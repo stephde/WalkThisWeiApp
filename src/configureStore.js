32ae207b263217bb81bcdc928db48c81ff9d4b26
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2017-05-03 11:05:34 +0200

    Add react-devtools-extension
    In order to make the react native debugger recognize our store, we have
    to compose the middleware with our devtools.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,16 +1,13 @@
 import { AsyncStorage } from 'react-native';
-import devTools from 'remote-redux-devtools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import reducer from './reducers';
 
 export default function configureStore(onCompletion) {
-  const enhancer = compose(
-    applyMiddleware(thunk),
-    devTools({
-      name: 'nativestarterkit', realtime: true,
-    }),
+  const enhancer = composeWithDevTools(
+    applyMiddleware(thunk)
   );
 
   const store = createStore(reducer, enhancer);
