d4c4706715ac2bef8950f9d413c94f1cc8122133
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2017-05-03 16:05:31 +0200

    Add margin for tabbar

--- src/app.js
+++ src/app.js
@@ -7,10 +7,27 @@ import MapContainer from './components/map/MapContainer';
 import Search from './search';
 import FilterView from './FilterView';
 
+
+const getSceneStyle = (props, computedProps) => {
+  const style = {
+    flex: 1,
+    backgroundColor: '#fff',
+    shadowColor: null,
+    shadowOffset: null,
+    shadowOpacity: null,
+    shadowRadius: null,
+  };
+  if (computedProps.isActive) {
+    style.marginTop = computedProps.hideNavBar ? 0 : 64;
+    style.marginBottom = computedProps.hideTabBar ? 0 : 50;
+  }
+  return style;
+};
+
 export default class App extends Component {
   render() {
     return (
-      <Router>
+      <Router getSceneStyle={getSceneStyle}>
         <Scene key="root">
           <Scene key="tabbar" tabs={true} hideNavBar={true} tabBarStyle={{ backgroundColor: '#FFFFFF' }}>
             <Scene
