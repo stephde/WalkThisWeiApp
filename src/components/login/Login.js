0e778fc997890de6d7b48149be2655790edce2a4
Janos Brauer <janos.brauer@student.hpi.uni-potsdam.de>
2017-05-13 17:47:33 +0200

    Adds password validation

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,41 +2,43 @@
 
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Button, Container, Form, Input, Item, Text } from 'native-base';
+import { Button, Container, Form, Input, Item, Text, Toast } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import PhoneInput from 'react-native-phone-input'
 import { connect } from 'react-redux';
 import { setUser } from '../../actions/';
 
-// import styles from './styles';
-class InputPhone extends Component {
-  render() {
-    return (
-      <Form style={{flex: 1}}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{justifyContent: 'center'}}>
-            <Item style={{borderColor: '#FFFFFF', borderBottomWidth: 2.5, borderBottomColor: '#FFFFFF'}}>
-              <PhoneInput ref='phone' style={{height: 50}} flagStyle={{height: 18, resizeMode: 'contain'}}/>
-            </Item>
-          </View>
-          <View style={{flex: 0.8}}>
-            <Item style={{borderColor: '#FFFFFF', borderBottomWidth: 2.5, borderBottomColor: '#FFFFFF'}}>
-              <Input keyboardType="phone-pad" placeholderTextColor="#FFFFFF" placeholder="Phone Number" style={{fontSize: 18, color: '#FFFFFF'}}/>
-            </Item>
-          </View>
-        </View>
-      </Form>
-    );
-  }
-}
-
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneNumber: '',
+      isPhoneInputValid: true
+    }
+  }
+
   onLogin() {
+    if(!this.state.isPhoneInputValid || !this.state.phoneNumber) {
+      Toast.show({
+             supportedOrientations: ['landscape', 'portrait'],
+             text: 'Wrong phone number!',
+             position: 'top',
+             buttonText: 'Okay'
+           });
+      return;
+    }
     this.props.setUser("test");
   }
+  onPhoneNumberChange(number) {
+    this.setState({
+      isPhoneInputValid: this.refs.phone.isValidNumber(),
+      phoneNumber: number
+    });
+  }
 
   render() {
+    const inputBorderColor = this.state.isPhoneInputValid ? '#FFFFFF' : 'red';
     return (
       <Container style={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#70C8BE', paddingTop: 20}}>
         <Grid>
@@ -44,12 +46,21 @@ class Login extends Component {
             <Text style={{fontSize: 34, color: '#FFFFFF'}}>Sign Up with us</Text>
           </Row>
           <Row size={40} style={{justifyContent: 'center', alignItems: 'center'}}>
-            <InputPhone/>
+            <View style={{justifyContent: 'center', flex: 1}}>
+              <Item style={{borderColor: '#FFFFFF', borderBottomWidth: 2.5, borderBottomColor: inputBorderColor}}>
+                <PhoneInput
+                  ref='phone'
+                  style={{flex: 1, height: 50}} flagStyle={{height: 18, resizeMode: 'contain'}}
+                  textProps={{placeholder: 'Phone Number', placeholderTextColor: '#FFFFFF'}}
+                  textStyle={{flex: 1, color: '#FFFFFF'}}
+                  onChangePhoneNumber={(number) => {this.onPhoneNumberChange(number);}}/>
+              </Item>
+            </View>
           </Row>
           <Row size={30} style={{alignItems: 'center', justifyContent: 'center'}}>
             <View>
-              <Button transparent style={{borderWidth: 3, borderColor: '#FFFFFF', borderRadius: 99, height: 70, width: 170, justifyContent: 'center'}} onPress={() => {this.onLogin();}}>
-                <Text style={{fontSize: 22, color: '#FFFFFF'}}>Login</Text>
+              <Button transparent style={{borderWidth: 3, borderColor: inputBorderColor, borderRadius: 99, height: 70, width: 170, justifyContent: 'center'}} onPress={() => {this.onLogin();}}>
+                <Text style={{fontSize: 22, color: inputBorderColor}}>Login</Text>
               </Button>
             </View>
           </Row>
