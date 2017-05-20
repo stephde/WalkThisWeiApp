9e358025f03ea1234f0a4297cb212f280ad50bd3
Janos Brauer <janos.brauer@student.hpi.uni-potsdam.de>
2017-05-20 12:10:16 +0200

    Adds react-native-ble-plx package

--- android/app/src/main/java/com/walkthisweiapp/MainApplication.java
+++ android/app/src/main/java/com/walkthisweiapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.walkthisweiapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.audioStreaming.ReactNativeAudioStreamingPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlePackage(),
             new RNDeviceInfo(),
             new ReactNativeAudioStreamingPackage(),
             new MapsPackage(),
